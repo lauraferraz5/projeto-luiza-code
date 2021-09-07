840671c279efd471140567cc15ab2108780f6f71
TamiresLucena <tamiresb.lucena@gmail.com>
2021-09-07 18:52:54 -0300

    adição de item em lista de compra

--- src/services/listas.js
+++ src/services/listas.js
@@ -1,16 +1,24 @@
+const { produto } = require("../models");
+
 class ListaService {
     constructor(ListaModel) {
         this.lista = ListaModel;
     }
 
-    // async get(id) {
-    // const listas = await this.lista.find(
-
-    // );
-    // return listas;
-    // }
-
-
+    async adicionar({ clienteId, produtoId, lojaId }) {
+        let lista = await this.lista.findOne({
+            where: {
+                ClienteId: clienteId,
+                LojaId: lojaId,
+                status: 'Em andamento'
+            },
+        });
+        if (lista == null) {
+            lista = await this.lista.create({ ClienteId: clienteId, LojaId: lojaId, status: 'Em andamento' })
+        }
+        const prod = await produto.findByPk(produtoId)
+        await lista.addProduto(prod)
+    }
 }
 
 module.exports = ListaService;
