1fcacd36694c745adfb3c1618928e04819b07ef1
marinajulia <marinajulia707@gmail.com>
2021-09-12 12:24:14 -0300

    Configurando DB

--- src/config/database.js
+++ src/config/database.js
@@ -6,4 +6,10 @@ module.exports = {
   database: "dc6scnn8k031v7",
   username: "mtgzqvmvpmhjxk",
   password: "ec1776f2703b70831b97ef6a02a5b456e037dac12981ab75673824eca2364b93",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+    }
+  },
 };
