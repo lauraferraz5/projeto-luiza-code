5f9349c1d2a9a6edc8749f8c2d140239f9824684
marinajulia <marinajulia707@gmail.com>
2021-09-12 12:12:12 -0300

    Configurando DB

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: "postgres",
-  host: "localhost",
+  host: "ec2-3-218-149-60.compute-1.amazonaws.com",
   port: 5432,
   // database: "projetoLuizaCode3",
-  database: "postgres",
-  username: "postgres",
-  password: "root",
+  database: "dc6scnn8k031v7",
+  username: "mtgzqvmvpmhjxk",
+  password: "ec1776f2703b70831b97ef6a02a5b456e037dac12981ab75673824eca2364b93",
 };
