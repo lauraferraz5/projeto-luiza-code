df7d4034bbcb7e8c85fbd5594a965990c883d97a
TamiresLucena <tamiresb.lucena@gmail.com>
2021-09-12 20:43:57 -0300

    adicionando swagger

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const routers = require("./api");
 const { sequelize } = require("./models");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger_output.json");
+
 const app = express();
-// const swaggerUi = require("swagger-ui-express");
-// const swaggerFile = require("./swagger_output.json");
 
 app.use(express.json());
 app.use("/", routers);
-// app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 //{ force: true }
 sequelize.sync().then(() => {
