cac22b6895e6667888b27856c20442330cfc31b3
marinajulia <marinajulia707@gmail.com>
2021-09-12 22:41:21 -0300

    config swagger

--- src/app.js
+++ src/app.js
@@ -15,6 +15,6 @@ sequelize.sync().then(() => {
   console.log("Conectado com o banco com sucesso!");
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3002, () => {
   console.log("Servidor ativo na porta 3002!");
 });
